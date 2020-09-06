429b64d469d5bb92fb0548080809b9bde94308f3
srijani157 <59190808+srijani157@users.noreply.github.com>
2020-09-07 01:54:16 +0530

    update

--- src/server.js
+++ src/server.js
@@ -13,6 +13,4 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen((process.env.PORT || 8080), function(){
-  console.log('listening on *:5000');
-});
+app.listen(port);
